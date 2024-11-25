e19a7828181db1e3b61958e9ebc850be8a0e44a7
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-11-25 14:55:48 -0500

    ADDED: solution for leedcode challenge 2

--- challenge-JS/2-greatest-common-divisor.js
+++ challenge-JS/2-greatest-common-divisor.js
@@ -18,4 +18,28 @@ str1 and str2 consist of English uppercase letters.
 
 /*
 APPROACH:
+- Base if: Make sure two strings can be added in both ways (str 1 + str 2 should equal str 2 + str 1), if not then words are not made up of same letters and will have no common letters
+- Find the length of str1 and str2, and store it in variables for easier readability
+- Determine the shorter of the two string lengths using Math.min and store value in a variable, this helps define the maximum possible length of the common substring.
+- Start a for loop from minLength and decrement until 1 (we want the largest common substring).
+- Inside the loop, check if both str1.length and str2.length % l === 0 to ensures that a substring of length l can divide both strings equally without remainders.
+- If true, return substring from the start of str1 with length l using str1.substring(0, l) to get common substring
 */
+
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) {
+    return "";
+  }
+  let minLength = Math.min(str1.length, str2.length);
+
+  for (let l = minLength; l > 0; l--) {
+    if (str1.length % l === 0 && str2.length % l === 0) {
+      return str1.substring(0, l);
+    }
+  }
+};
